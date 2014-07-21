45989c1531b1e4b9878efa1790dd2b206698f044
HoldenCaulfieldICL <adalyac@gmail.com>
2014-07-21 19:20:48 +0100

    create_lookup_txtfiles ready to test

--- scripts/data_preparation/create_lookup_txtfiles_2.py
+++ scripts/data_preparation/create_lookup_txtfiles_2.py
@@ -28,48 +28,35 @@ def get_label_dict(data_dir):
 def create_lookup_txtfiles(data_dir, to_dir=None):
   ''' data_dir: where raw data is
       to_dir: where to store .txt files. '''
-
-  case_count = 0  # number of training cases
-  tagless_count = 0 # n
-  badcase_count = 0 # num of images with multiple flags to train on
   All = get_label_dict(data_dir)
-  count = {}
-
   Keep = classes_to_learn(All)
   # merge_classes only after default label entry created
   Keep = default_class(All, Keep)
   Keep = merge_classes(Keep)
   Keep = shuffle_and_rebalance(Keep)
-
   if to_dir is not None:
     dump_to_files(Keep)
-
   return Keep
 
-def dump_to_files(Keep):
-  train_file = open(ojoin(to_dir,'train.txt'), 'w')
-  val_file = open(ojoin(to_dir,'val.txt'), 'w')
-  test_file = open(ojoin(to_dir,'test.txt'), 'w')
-  read_file = open(ojoin(to_dir,'read.txt'), 'w')
 
-  lookup, dump = {}, []
-  for (num,key) in enumerate(Keep.keys()):
-    lookup[key] = num
-    dump += ["%s %i\n" % (f,key) for f in Keep[key]]
-
-  train_file.writelines(["%s %i\n" % (fname,lookup[k])
-                         for (fname,num) in train_dump])
-  val_file.writelines(["%s %i\n" % (fname,num)
-                       for (fname,num) in val_dump])
-  test_file.writelines(["%s %i\n" % (fname,num)
-                        for (fname,num) in test_dump])
-
-  # write to read file how to interpret values as classes
-  read_file.writelines(["%i %s\n" % (lookup[label],label,)
-                        for label in write_labels])
-  train_file.close()
-  val_file.close()
-  test_file.close()
+def dump_to_files(Keep, to_dir):
+  dump_fnames = ['train.txt','val.txt','test.txt']
+  part = [0, 0.8, 0.87, 1] # partition into train val test
+  for i in xrange(3):
+    dfile = open(ojoin(to_dir,dump_fnames[i]),'w')
+    dump = []
+    for (num,key) in enumerate(Keep.keys()):
+      l = len(Keep[key])
+      dump += ["%s %i\n" % (f,num) for f in
+               Keep[key][part[i]*l:part[i+1]*l]]
+    random.shuffle(dump)
+    dfile.writelines(dump)
+    dfile.close()
+    
+  # write to read file how to interpret values as classes      
+  read_file = open(ojoin(to_dir,'read.txt'), 'w')    
+  read_file.writelines(["%i %s\n" % (num,label) for (num, label)
+                         in enumerate(Keep.keys())])
   read_file.close()
 
     
@@ -132,94 +119,7 @@ def classes_to_learn(All):
     Keep[label] = All[label]
   return Keep
   
-#### STEP 5.1: SETUP IMBALANCE EXPERIMENT ############################
-
-def rebalance(D, min_ratio, max_ratio, num_nets):
-  ''' given a dict containing fnames for each class, a 
-  range of imbalance ratios to cover, and a number of nets to train,
-  creates num_nets directories, each holding a subdir for each class
-  , with max_ratio as imbalance for net_0, ..., min_ratio as 
-  imbalance for net_num_nets. '''
-
-  if min_ratio < 1 or max_ratio < 1: 
-    print 'Error: ratios must be >=1.'
-    exit
-
-  # using cool log calculus, compute la raison de la suite 
-  # geometrique donnant les ratios a obtenir pour chaque net.
-  step = compute_step(min_ratio, max_ratio, num_nets)
-
-  # move contents of data_dir to a new subdir, 'all'
-  if os.path.isdir(ojoin(data_dir,'all')):
-    shutil.rmtree(ojoin(data_dir,'all'))
-  all_names = os.listdir(data_dir)
-  for name in all_names:
-    shutil.move(ojoin(data_dir,name), ojoin(data_dir,'all',name))
-
-  # recursively make subdirs for each net, preserving strict set 
-  # inclusion from net[i] to net[i+1]
-  nets = ['all'] + ['net_'+str(i) for i in range(num_nets)]
-  random_delete_recursive(data_dir, step, nets, ratio=2, i=0)
-  print 'NOTE: net_0 has highest imbalance ratio.'
-
-
-def random_delete_recursive(data_dir, step, nets, ratio, i):
-#  os.mkdir(ojoin(data_dir,nets[i+1]))
-  if os.path.isdir(ojoin(data_dir,nets[i+1])):
-    shutil.rmtree(ojoin(data_dir,nets[i+1]))
-  shutil.copytree(ojoin(data_dir, nets[i]), 
-                  ojoin(data_dir, nets[i+1]), symlinks=True)
-  random_delete_aux(ojoin(data_dir, nets[i+1]), ratio)
-  if i+2 in range(len(nets)):
-    random_delete_recursive(data_dir, step, nets, float(ratio)/step, i+1)
-
-
-# careful! if you deleted links and now wish to add some back, make 
-# sure json dump gets updated/overwritten correctly 
-def random_delete_aux(data_dir, ratio):
-  ''' randomly deletes as few images from outnumbering class dirs 
-      as possible such that #biggest/#smallest == ratio. '''
-
-  data_dir = os.path.abspath(data_dir)
-  dump = raw_input('Do you want a json dump in %s of which files were randomly deleted?(Y/any) '%(data_dir))
-    
-  # D is for dict, d is for directory
-  D = {}
-  os.chdir(data_dir)
-  dirs = [d for d in os.listdir(data_dir) if os.path.isdir(ojoin(data_dir,d))]
-  
-  print 'the directories are: %s'%(dirs)
-
-  for d in dirs:
-    D[d] = {}
-    D[d]['total'] = len(os.listdir(ojoin(data_dir,d)))
-
-  dirs = [(d,D[d]['total']) for d in D.keys()]
-  dirs = sorted(dirs, key = lambda x: x[1])
-
-  print '%s is smallest class with %i images'%(dirs[0][0],dirs[0][1])
-  for d in D.keys():
-    D[d]['remove'] = max(0,int(D[d]['total']-(ratio*dirs[0][1])))
-    print '%s has %i images so %i will be randomly removed'%(d, D[d]['total'], D[d]['remove'])
-    if D[d]['remove'] > 0 :
-      D = random_delete_aux2(data_dir,d,D)
-
-  if dump == 'Y': json.dump(D, open(data_dir+'/random_remove_dict.txt','w'))
-  return D
-
-
-# D is for dict, d is for directory
-def random_delete_aux2(data_dir,d,D,delete_hard=False):
-  D[d]['deleted'] = random.sample(os.listdir(ojoin(data_dir,d)),D[d]['remove'])
-  print 'successfully condemned images from %s'%(d)
-  back = os.getcwd()
-  os.chdir(ojoin(data_dir,d))
-  for link in D[d]['deleted']: os.remove(link)
-  os.chdir(back)
-  return D
-
-
-def compute_step(min_ratio, max_ratio, num_nets):
-  ''' calculates step such that ratio[i+1] = ratio[i]*step for all i,   and such that '''
-  return pow(max_ratio/float(min_ratio), 1/float(num_nets-1))
+if __name__ == '__main__':
+  import sys
+  create_lookup_txtfiles(sys.argv[1],sys.argv[2])
 
