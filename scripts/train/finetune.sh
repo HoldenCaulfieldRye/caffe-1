4143816faf3e8622c9b5f111bc6e1611f0834a30
HoldenCaulfieldICL <adalyac@gmail.com>
2014-07-25 03:42:18 +0100

    update

--- scripts/train/finetune.sh
+++ scripts/train/finetune.sh
@@ -11,7 +11,7 @@ set -e
 
 
 
-for TASK_NAME in scrape_zone_peel; do
+for TASK_NAME in markings; do
 
     # with 4, bad minimum provides 80% classification accuracy
     # read -p "Target bad min? (e.g. 0.8 for class imbalance such that 80% a bad/fake minimum yields 80% accuracy) "
