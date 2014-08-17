f593373e977178c56bceb87cb26bccc00f82801b
alex <adalyac@gmail.com>
2014-08-17 21:12:16 +0200

    removing _fine from prototxt contents

--- models/clampdet/resume_training.sh
+++ models/clampdet/resume_training.sh
@@ -3,6 +3,6 @@
 TOOLS=../../build/tools
 
 GLOG_logtostderr=1 $TOOLS/train_net.bin \
-    clampdet_fine_solver.prototxt clampfine_train_iter_4900.solverstate
+    clampdet_solver.prototxt clampfine_train_iter_4900.solverstate
 
 echo "Done."
