d58c0ac6e4e36e9bc272734854e87b9f83d22d3d
HoldenCaulfieldICL <adalyac@gmail.com>
2014-08-24 21:49:58 +0100

    deploy prototxt for scrape zone peel

--- python/create_deploy_prototxt.py
+++ python/create_deploy_prototxt.py
@@ -12,21 +12,35 @@ def get_train_file(model_dir):
   sys.exit()
 
 def edit_train_content_for_deploy(content):
-  for idx in range(len(content)):
-    if 'blobs_lr' in line: del content[idx]
-    elif 'weight_decay' in line: del content[idx]
-    elif 'weight_filler' in line: del content[idx:idx+7]
-    elif 'accuracy' in line: del content[idx-1:idx+5]
+  idx = 0
+  while idx < len(content):
+    if 'blobs_lr' in content[idx]:
+      del content[idx]
+      # idx += 1
+    elif 'weight_decay' in content[idx]:
+      del content[idx]
+      # idx += 1
+    elif 'weight_filler' in content[idx]:
+      del content[idx:idx+7]
+      idx += 7
+    elif 'accuracy' in content[idx]:
+      del content[idx-1:idx+5]
+      idx += 5
+    else:
+      idx += 1
   return content
 
 def write_content_to_deploy_file(model_dir, content):
   model_name = model_dir.split('/')[-1]
-  deploy_file = open(ojoin(model_dir,model_name+'_deploy.prototxt'),'w')
+  model_name = model_name.split('-fine')[0]
+o  fname = ojoin(model_dir,model_name+'_deploy.prototxt')
+  print "fname: %s"%(fname)
+  deploy_file = open(fname,'w')
   deploy_file.writelines(content)
   deploy_file.close()
 
 if __name__ == '__main__':
-  model_dir = os.abspath(sys.argv[2])
+  model_dir = os.path.abspath(sys.argv[1])
   train_file = get_train_file(model_dir)
 
   content = train_file.readlines()
