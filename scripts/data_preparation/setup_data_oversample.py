432cc5d0b5aa74e5db5d2a8c8b1d9e7405d66100
HoldenCaulfieldICL <adalyac@gmail.com>
2014-08-28 01:27:20 +0100

    oversample ready

--- scripts/data_preparation/setup_data_oversample.py
+++ scripts/data_preparation/setup_data_oversample.py
@@ -6,6 +6,7 @@ from operator import itemgetter as ig
 from itertools import chain
 from datetime import date
 from shutil import rmtree
+import copy
 import json, yaml, random
 import setup_data
 
@@ -54,22 +55,54 @@ def rebalance_oversample(Keep, total_num_images, target_bad_min):
   # print ascending_classes
   # print "\ntotal num images: %i"%(total_num_images)
   minc_proportion = float(len_minc)/total_num_images
+  maxc_proportion = float(len_maxc)/total_num_images
   if target_bad_min is None:
     target_bad_min = raw_input("\nmax class currently takes up %.2f, what's your target? [num/N] "%(maxc_proportion))
   if target_bad_min is not 'N':
     target_bad_min = float(target_bad_min)
     print 'minc_proportion: %.2f, target_bad_min: %.2f'%(minc_proportion, target_bad_min)
     if maxc_proportion > target_bad_min:
-      copy_size = ( target_bad_min*(total_num_images) - len_minc ) / (1 - target)
+      copy_size = ( target_bad_min*(total_num_images) - len_minc ) / (1 - target_bad_min)
+      copy_size = int(copy_size)
       random.shuffle(Keep[minc])
       print '%s has %i images so %i copies will be made'%(minc, len_maxc,copy_size)
-      copy = Keep[minc].copy()
-      for i in range((copy_size/len_minc)-1):
-        Keep[minc].append(copy)
-      Keep[minc].append(copy[:(copy_size % len_minc)]])
+      min_imgs_copy = copy.copy(Keep[minc])
+      print 'min_imgs_copy has %i images'%(len(min_imgs_copy))
+      # number_of_copies = 
+      # number_of_copies = int(number_of_copies)
+      for i in range((copy_size/len_minc)):
+        Keep[minc] += min_imgs_copy
+      Keep[minc] += min_imgs_copy[:(copy_size % len_minc)]
       random.shuffle(Keep[minc])
-      total_num_images = 0
-      for key in Keep.keys():
-        total_num_images == len(Keep[key])
-      assert len(Keep[maxc])/float(total_num_imgs) == target_bad_min
+      total_num_images = len(Keep[minc]) + len(Keep[maxc])
+      print 'minc now has %i images, compared to %i for maxc'%(len(Keep[minc]), len(Keep[maxc]))
+      assert len(Keep[maxc])/float(total_num_images) == target_bad_min
   return Keep
+
+
+if __name__ == '__main__':
+  import sys
+  
+  target_bad_min, data_dir, data_info = None, None, None
+  for arg in sys.argv:
+    if "bad-min=" in arg:
+      target_bad_min = arg.split('=')[-1]
+      print "target bad min: %s" %(target_bad_min)
+    elif "data-dir=" in arg:
+      data_dir = os.path.abspath(arg.split('=')[-1])
+    elif "to-dir=" in arg:
+      to_dir = os.path.abspath(arg.split('=')[-1])
+    elif "data-info=" in arg:
+      data_info = os.path.abspath(arg.split('=')[-1])
+
+  # data_dir = /data/ad6813/pipe-data/Bluebox/raw_data/dump
+  # data_info = /data/ad6813/caffe/data_info
+  if data_dir is None:
+    print "\nERROR: data_dir not given"
+    exit
+      
+  num_output,dump = main(data_dir, data_info, to_dir, target_bad_min)
+  print "\nIt's going to say 'An exception has occured etc'"
+  print "but don't worry, that's num_output info for the training shell script to use\n"
+  sys.exit(num_output)
+
