0fdbbd2d249e8c39874637e81a5bb15f9db04762
HoldenCaulfieldICL <adalyac@gmail.com>
2014-09-09 04:39:04 +0100

    train update

--- models/soil_contam/resume_training.sh
+++ models/soil_contam/resume_training.sh
@@ -3,6 +3,6 @@
 TOOLS=../../build/tools
 
 GLOG_logtostderr=1 $TOOLS/train_net.bin \
-    soil_contam_solver.prototxt soil_contam_fine_train_iter_2000.solverstate
+    soil_contam_solver.prototxt soil_contam_fine_train_iter_5000.solverstate
 
 echo "Done."
