9ba183160a9b3ebefe754d14e66d1886c09a0680
HoldenCaulfieldICL <adalyac@gmail.com>
2014-09-12 20:51:53 +0100

    nice

--- models/clampdet/fine_clampdet.sh
+++ models/clampdet/fine_clampdet.sh
@@ -4,4 +4,9 @@ TOOLS=../../build/tools
 
 GLOG_logtostderr=1 $TOOLS/finetune_net.bin clampdet_solver.prototxt ../alexnet/caffe_alexnet_model
 
+echo "Done training."
+echo "Deleting leveldbs..."
+
+rm -rf *leveldb
+
 echo "Done."
