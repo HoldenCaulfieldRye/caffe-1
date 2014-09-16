12eb8260239706950716da6836ef0203eaa02764
HoldenCaulfieldICL <adalyac@gmail.com>
2014-09-16 18:30:54 +0100

    script to get unsuitable flag

--- scripts/data_preparation/reorganizeData.py
+++ scripts/data_preparation/reorganizeData.py
@@ -1,6 +1,9 @@
 import sys
 import os
 import shutil
+from os.path import join as oj
+
+# expected input: python reorganizeData.py ../../../pipe-data/Bluebox
 
 def reorganize(baseDir):
   dirs = filter(lambda x: x.isdigit(), os.listdir(baseDir))
@@ -16,7 +19,20 @@ def reorganize(baseDir):
       shutil.move(cdr+"/"+jpg, tdr+"/"+jpg) 
       shutil.copyfile(cdr+"/inspection.txt", tdr+"/"+name+".dat")
       shutil.copyfile(cdr+"/meta.txt", tdr+"/"+name+".met")
+      getUnsuitableFlags(tdr, name)
     shutil.rmtree(cdr) 
  
+def getUnsuitableFlags(tdr, name):
+  with open(tdr+"/"+name+".met") as met_f:
+    if any(["UnsuitablePhoto=True" in line for line in met_f.readlines()]):
+      with open(tdr+"/"+name+".dat", 'a') as dat_f:
+        dat_f.write("UnsuitablePhoto")
+
 if __name__ == "__main__":
-  reorganize(sys.argv[1])
+  tdr = baseDir + "/raw_data/dump"
+  if os.path.isdir(tdr):
+    jpgs = filter(lambda x: "jpg" in x, os.listdir(tdr))
+    for jpg in jpgs:
+      getUnsuitableFlags(tdr, name)
+  else: reorganize(sys.argv[1])
+
