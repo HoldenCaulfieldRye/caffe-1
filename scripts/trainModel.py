a8a1bbfc5bdec533582b496a9b6134c19a15a3f7
HoldenCaulfieldICL <adalyac@gmail.com>
2014-09-22 09:59:27 +0100

    modified trainModel.py

--- scripts/trainModel.py
+++ scripts/trainModel.py
@@ -5,15 +5,30 @@ import getopt
 import copy
 
 if __name__ == "__main__":
-  opts, extraparams = getopt.gnu_getopt(sys.argv[1:], "", ["model=", "freq_snaps", "train_net=", "test_net=", "test_iter=", "test_interval=", "test_compute_loss=", "base_lr=", "display=", "max_iter=", "lr_policy=", "gamma=", "power=", "momentum=", "weight_decay=", "stepsize=", "snapshot=", "snapshot_prefix=", "snapshot_diff=", "solver_mode=", "device_id=", "random_seed="])
+  opts, extraparams = getopt.gnu_getopt(sys.argv[1:], "", ["train_batch=", "test_batch=", "max_test_iter", "model=", "freq_snaps", "train_net=", "test_net=", "test_iter=", "test_interval=", "test_compute_loss=", "base_lr=", "display=", "max_iter=", "lr_policy=", "gamma=", "power=", "momentum=", "weight_decay=", "stepsize=", "snapshot=", "snapshot_prefix=", "snapshot_diff=", "solver_mode=", "device_id=", "random_seed="])
+  nonSolverOpts = set(["max_test_iter", "model", "freq_snaps", "train_batch", "test_batch", "freeze_to", "reinit_from"])
   optDict = dict([(k[2:],v) for (k,v) in opts])
   print optDict
+
   if not "model" in optDict:
     raise Exception("Need to specify --model flag")
   task = optDict["model"]
   baseDir = os.path.abspath("../models/" + task) + "/"
   solverFile = baseDir + task + "_solver.prototxt"
   curOpts = [] 
+
+
+  test_batch = 96
+  if "test_iter" in optDict and "max_test_iter" in optDict:
+    print "WARNING: \"test_iter\" was specified, ignoring \"max_test_iter\" flag"
+    del optDict["max_test_iter"]
+  if "max_test_iter" in optDict:
+    with open("../data_info/" + task + "/val.txt", 'r') as f:
+      noVal = f.read().strip().split('\n')
+    test_iter = (noVal / test_batch) + 1
+    #print "Changing \"test_iter\" from", optDict["test_iter"], "to", test_iter
+    optDict["test_iter"] = test_iter
+      
   if "snapshot" in optDict and "freq_snaps" in optDict:
     print "WARNING: \"snapshot\" was specified, ignoring \"freq_snaps\" flag"
     del optDict["freq_snaps"]
@@ -30,17 +45,23 @@ if __name__ == "__main__":
       if opt in optDict:
         print "Changing", opt, "from", val, "to", optDict[opt]
         val = optDict[opt]
+        del optDict[opt]
       curOpts.append((opt,val))
 
-  if "snapshot" not in optDict and "freq_snaps" in optDict:
+  if "freq_snaps" in optDict:
     for i in range(len(curOpts)):
      if curOpts[i][0] == "snapshot":
        print "Changing \"snapshot\" from", curOpts[i][1], "to", test_interval
        curOpts[i] = (curOpts[i][0], test_interval)
 
+  for k,v in optDict.items():
+    print "Adding " + k + ": " + v + " to curOpts"
+    curOpts.append((k,v))
+
   with open (solverFile, 'w') as f:
     for k,v in curOpts:
-      f.write(k + ": " + v + "\n")
+      if k not in nonSolverOpts:
+        f.write(k + ": " + v + "\n")
   
   cmd = "cd " + baseDir + "; nohup ./fine_" + task + ".sh 2>&1 | tee train_output.log" 
   p = subprocess.Popen(cmd, shell=True, stdout = subprocess.PIPE, stderr=subprocess.STDOUT)
